3d45af563b38bab0ea648a57e52793465a389c63
Sebastian Lupa <61464256+lupasebastian@users.noreply.github.com>
2020-10-23 13:06:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+"""
+Simple but useful GUI application telling user to never hard
+reset not committed repo
+"""
+import PySimpleGUI as sg
+import pyttsx3 as speech
+from PySimpleGUI import Window
+
+
+def get_window(window_type) -> Window:
+    """returns a window object basing on the desired type"""
+    if window_type == 'choice':
+        layout = get_layout('choice')
+        window = sg.Window('Enter your choice', layout)
+    elif window_type == 'wrong_choice':
+        layout = get_layout('wrong_choice')
+        window = sg.Window('Wrong choice.', layout)
+    else:
+        layout = get_layout('do_not_reset!')
+        window = sg.Window('Do not reset!', layout)
+    return window
+
+
+def get_layout(layout_type) -> list:
+    """returns a layout schema for window creation"""
+    if layout_type == 'choice':
+        layout = \
+            [
+                [
+                    sg.Text('How would you like to be told not to reset '
+                            'never committed repo?\n'
+                            '1 - with text\n'
+                            '2 - with speech:\n')
+                ],
+                [sg.Input()],
+                [sg.Button('OK')]
+            ]
+    elif layout_type == 'wrong_choice':
+        layout = \
+            [
+                [
+                    sg.Text('Wrong choice, please enter again.')
+                ],
+                [sg.Button('OK')]
+            ]
+    else:
+        layout = \
+            [
+                [
+                    sg.Text('Do not ever make a hard reset on '
+                            'never committed repo!')
+                ],
+                [sg.Button('OK')]
+            ]
+    return layout
+
+
+def ask_for_decision() -> str:
+    """asks user on how they would like to be informed"""
+    event, decision = get_window('choice').read()
+    while True:
+        if decision[0].casefold() in ('1', '2'):
+            return decision[0]
+        get_window('wrong_choice').read()
+        event, decision = get_window('choice').read()
+
+
+if __name__ == '__main__':
+    final_decision = ask_for_decision()
+    if final_decision == '1':
+        get_window('do_not_reset').read()
+    if final_decision == '2':
+        engine = speech.init()
+        engine.say('Do not ever make a hard reset on never committed repo!')
+        engine.runAndWait()
